5e03bb2b05a242bd29683a5b455fefa4444619a4
cse-neeraj <cse.neerajyadav@gmail.com>
2025-09-29 18:44:06 +0000

    variables

---
+++ 01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 145251
+let accountEmail = "neerajyadav@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+// accountId = 2
+/*Prefer not to use var because of issue in block scope and functional scope
+ */
+
+accountEmail = "hsjs@mail"
+accountPassword = "5458"
+accountCity = "goa"
+
+console.table(accountId);
+console.table(accountEmail);
+console.table(accountPassword);
+console.table(accountCity);
+
+
---
+++ 02_datatypes.js
@@ -0,0 +1,17 @@
+ "use strict"; // treat all JS code as newer version
+ 
+ console.log(3+3)
+ console.log("Neeraj")
+
+ //number 
+ //big int
+ //string=>""
+ //boolean=> true/false
+ //null=> stand alone value
+ //undefined=>
+// symbol=> unique
+
+
+ // object
+  let x =  null;
+ console.log(typeof null);
